03fb8f26cf7e350e97cd30780ffa6c7e6e60a562
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-07-25 15:44:37 +0100

    chore: initialize Spring Boot project structure

---
+++ backend/src/test/java/com/mohcine/banqueApp/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohcine.banqueApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
