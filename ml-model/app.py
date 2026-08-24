63dcac70517c58326f84d7a0811224c5ed66bb70
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-08-24 21:54:03 +0100

    adding some tests to the model to verify the effeciency

--- ml-model/app.py
+++ ml-model/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 model = joblib.load("nn_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
@@ -17,18 +18,36 @@ def predict():
     duree = data["duree"]
     taux = data["taux"]
 
+    # Préparation des données
     X = np.array([[revenu, remboursement, duree, taux]])
     X_scaled = scaler.transform(X)
 
-    proba = model.predict_proba(X_scaled)[0][1]
+    # Prédiction
+    probabilities = model.predict_proba(X_scaled)[0]
     prediction = model.predict(X_scaled)[0]
+
+    # Probabilité de la classe 1
+    proba = probabilities[1]
 
     decision = "RISQUE_ELEVE" if prediction == 1 else "RISQUE_FAIBLE"
 
+    
+
+    # Debug
+    print("===================================")
+    print("Input :", X)
+    print("Input scaled :", X_scaled)
+    print("Probabilities :", probabilities)
+    print("Prediction :", prediction)
+    print("Risk probability :", proba)
+    print("Decision :", decision)
+    print("===================================")
+
     return jsonify({
-        "score_risque": round(float(proba), 2),
+        "score_risque": float(proba),
         "decision": decision
     })
+
 
 if __name__ == "__main__":
     app.run(port=5000)
