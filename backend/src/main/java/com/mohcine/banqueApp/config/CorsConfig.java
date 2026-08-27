750ad312b8b2c4b92fb8d545c2ae60d30a900bd2
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-08-27 04:35:37 +0100

    adding CorsConfig and allowing only the requests arriving from my client side  http://localhost:5176/

--- backend/src/main/java/com/mohcine/banqueApp/config/CorsConfig.java
+++ backend/src/main/java/com/mohcine/banqueApp/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                         .allowedHeaders("*")
-                        .allowedOrigins("http://localhost:4200");
+                        .allowedOrigins("http://localhost:5176");
             }
         };
     }
